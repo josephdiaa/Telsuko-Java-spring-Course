2ed74951b7d29681cef0faed0e74a0696c8081fd
josephdiaa <81682206+josephdiaa@users.noreply.github.com>
2025-05-19 16:34:48 +0300

    modifiy

--- Demo.java
+++ Demo.java
@@ -1,11 +1,3 @@
-public class calculator{
-    public int add(int n1, int n2){
-        return n1+n2;
-    }
-}
-
-
-
 public class Demo{
     public static void main(String args[]){
         int nums[]={3,7,2,4};
