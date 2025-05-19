880d1bcd2b20857c5399d70391419974f7011274
josephdiaa <81682206+josephdiaa@users.noreply.github.com>
2025-05-19 16:28:45 +0300

    first class

---
+++ project.java
@@ -0,0 +1,14 @@
+public class calculator{
+    public int add(int n1, int n2){
+        return n1+n2;
+    }
+}
+
+
+
+public class Demo{
+    public static void main(String args[]){
+        int nums[]={3,7,2,4};
+        System.out.println(nums[0]);
+    }
+}
